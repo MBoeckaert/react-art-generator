e855ab243ad1d7ad9cc68f3f173f3941be77d412
Matthias <matthiasboeckaert@gmail.com>
2022-02-22 14:27:18 +0100

    checking smth

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          MBoeckaert Art Generator
+          MBoeckaert My Art Generator
         </p>
         <a
           className="App-link"
